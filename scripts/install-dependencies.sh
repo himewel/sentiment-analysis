3257812985c510f2b2338bec50f4eedcfe480e9e
Welbert Castro <himewel@users.noreply.github.com>
2022-03-10 19:14:59 -0300

    Add dev dependencies installation script

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get update
+sudo apt-get install --yes \
+    python3-pip \
+    python3-venv
+
+pip3 install --upgrade "jupyterlab>=3.3.0"
+
+sudo mkdir -p /opt/jupyterlab/etc/systemd
+sudo mv jupyterlab.service /opt/jupyterlab/etc/systemd/
+
+sudo ln -s /opt/jupyterlab/etc/systemd/jupyterlab.service /etc/systemd/system/jupyterlab.service
+sudo systemctl daemon-reload
+sudo systemctl enable jupyterlab.service
+sudo systemctl start jupyterlab.service
+sudo systemctl status jupyterlab.service
